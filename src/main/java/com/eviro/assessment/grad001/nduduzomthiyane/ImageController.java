0a557789aef7dd244e2975a0f98966f196593ed9
ndumthiy021 <ndumthiy021@student.wethinkcode.co.za>
2023-06-06 19:44:31 +0200

    controller class can now persist database lowercase on querying both name and surname

--- src/main/java/com/eviro/assessment/grad001/nduduzomthiyane/ImageController.java
+++ src/main/java/com/eviro/assessment/grad001/nduduzomthiyane/ImageController.java
@@ -1,6 +1,5 @@
 package com.eviro.assessment.grad001.nduduzomthiyane;
 
-
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -12,7 +11,6 @@ import java.net.URL;
 import java.sql.*;
 
 
-
 @RestController
 @RequestMapping("/v1/api/image")
 public class ImageController {
@@ -20,39 +18,37 @@ public class ImageController {
     String jdbcURL = "jdbc:h2:mem:images_db";
     Connection conn;
 
-    @GetMapping(value = "/{name}")
-    public ResponseEntity<FileSystemResource> getHttpImgLink(@PathVariable String name) throws SQLException, IOException {
+    @GetMapping(value = "/{name}/{surname}")
+    public ResponseEntity<FileSystemResource> getHttpImgLink(@PathVariable String name,
+                                                             @PathVariable String surname)
+                                                            throws SQLException, IOException {
 
-        URL url1 = new URL(persistDatabase(name.toLowerCase()));
+        URL url1 = new URL(persistDatabase(name.toLowerCase(),surname.toLowerCase()));
 
         FileSystemResource resource =
                 new FileSystemResource(url1.getPath());
         HttpHeaders headers = new HttpHeaders();
-
-
         headers.setContentType(MediaType.IMAGE_PNG);
         headers.setContentLength(resource.contentLength());
 
         return new ResponseEntity<>(resource, headers, HttpStatus.OK);
-        //String path = persistDatabase(name);
-        //System.out.println(path);
-        //return new FileSystemResource("/home/ndu/Documents/nduduzomthiyane/imagesDB/Momentum.png",);
+
     }
 
-    private String persistDatabase(String name) throws SQLException {
+    private String persistDatabase(String name,String surname) throws SQLException {
 
         conn = DriverManager.getConnection(jdbcURL);
 
         try( final Statement stmt = conn.createStatement() ){
             boolean gotAResultSet = stmt.execute(
-                    "SELECT httpImgLink FROM account_profile "
-                            + "WHERE name = '" + name + "';"
+                    "SELECT name, surname, httpImgLink FROM account_profile "
+                            + "WHERE name = '" + name + "' " +
+                            "AND surname = '" + surname + "';"
             );
             if( ! gotAResultSet ){
                 throw new RuntimeException( "Expected a SQL resultset, but we got an update count instead!" );
             }
             try( ResultSet results = stmt.getResultSet() ){
-                int rowNo = 1;
                 while( results.next() ){
                     return results.getString( "httpImgLink"
                     );}
