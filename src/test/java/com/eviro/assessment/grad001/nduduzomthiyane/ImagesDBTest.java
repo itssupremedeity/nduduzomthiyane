c859925ba45bd56516e26ac15247648df1569af8
ndumthiy021 <ndumthiy021@student.wethinkcode.co.za>
2023-06-08 01:44:05 +0200

    tests for the imagesDB class

--- src/test/java/com/eviro/assessment/grad001/nduduzomthiyane/ImagesDBTest.java
+++ src/test/java/com/eviro/assessment/grad001/nduduzomthiyane/ImagesDBTest.java
@@ -2,17 +2,22 @@ package com.eviro.assessment.grad001.nduduzomthiyane;
 
 import com.eviro.assessment.grad001.nduduzomthiyane.model.ImagesDB;
 import com.eviro.assessment.grad001.nduduzomthiyane.model.Img;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import java.io.*;
+import java.util.Optional;
 import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 
 public class ImagesDBTest {
 
-
     private ImagesDB parser;
     private LineNumberReader input;
-
     private Set<Img> fakeImgData;
 
     @BeforeEach
@@ -21,6 +26,46 @@ public class ImagesDBTest {
         parser = new ImagesDB();
         fakeImgData = parser.parseDataLines(input);
     }
+
+    @AfterEach
+    public void tearDown(){
+        fakeImgData = null;
+        parser = null;
+        input = null;
+    }
+
+    @Test
+    public void checkIfOurSetHasObjects() throws IOException {
+        assertNotEquals( 0, fakeImgData.size());
+    }
+
+    @Test
+    public void checkIfFirstLineSkipped() throws IOException {
+        assertNotEquals( 4, fakeImgData.size());
+    }
+
+    @Test
+    public void checkIfNameIsStoredCorrectly() throws IOException {
+        Img img = fakeImgData.iterator().next();
+        assertEquals( "Amatikulu", img.getName());
+    }
+
+    @Test
+    public void checkIfSurnameIsStoredCorrectly() throws IOException {
+        Img img = fakeImgData.iterator().next();
+        assertEquals( "Station", img.getSurname());
+    }
+
+
+    @Test
+    public void checkIfImgDataIsStoredCorrectly() throws IOException {
+        Img img = fakeImgData.iterator().next();
+        assertEquals( "-29.05111111", img.getImgEncode());
+    }
+
+
+
+
 
 
 
--- src/test/java/com/eviro/assessment/grad001/nduduzomthiyane/TestData.java
+++ src/test/java/com/eviro/assessment/grad001/nduduzomthiyane/TestData.java
@@ -1,11 +1,12 @@
 package com.eviro.assessment.grad001.nduduzomthiyane;
 
 public interface TestData {
-    public static final String CSV_DATA =
+
+    String CSV_DATA =
             """
             name,surname,imageFormat,imageData
             Amatikulu,Station,image/jpg,-29.05111111
-            Amatikulu,Town,image.png,-29.04666667
+            Amatikulu,Town,image/png,-29.04666667
             Botrivier,Station,image/png,-34.22666667
             """ ;
 }
