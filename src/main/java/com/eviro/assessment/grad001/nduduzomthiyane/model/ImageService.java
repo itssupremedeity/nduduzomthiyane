a4c547d263729e22a24c0e103c32aba5a77d7829
ndumthiy021 <ndumthiy021@student.wethinkcode.co.za>
2023-06-07 16:27:42 +0200

    inserting data back end repositories doesnt work1

--- src/main/java/com/eviro/assessment/grad001/nduduzomthiyane/model/ImageService.java
+++
@@ -1,5 +0,0 @@
-package com.eviro.assessment.grad001.nduduzomthiyane.model;
-
-public class ImageService {
-
-}
--- src/main/java/com/eviro/assessment/grad001/nduduzomthiyane/repository/ImageRepo.java
+++
@@ -1,8 +0,0 @@
-package com.eviro.assessment.grad001.nduduzomthiyane.repository;
-
-import com.eviro.assessment.grad001.nduduzomthiyane.model.Imag;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ImageRepo extends CrudRepository<Imag,Integer> {
-
-}
