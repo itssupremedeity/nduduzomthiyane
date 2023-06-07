6cef41d0fcc2aaa42e64653c8a0dcc828ed2c27e
ndumthiy021 <ndumthiy021@student.wethinkcode.co.za>
2023-06-07 14:20:01 +0200

    modifying the code suit repositories1

--- src/main/java/com/eviro/assessment/grad001/nduduzomthiyane/repository/ImageRepo.java
+++ src/main/java/com/eviro/assessment/grad001/nduduzomthiyane/repository/ImageRepo.java
@@ -1,8 +1,8 @@
 package com.eviro.assessment.grad001.nduduzomthiyane.repository;
 
-import com.eviro.assessment.grad001.nduduzomthiyane.model.imag;
+import com.eviro.assessment.grad001.nduduzomthiyane.model.Imag;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ImageRepo extends CrudRepository<imag,Integer> {
+public interface ImageRepo extends CrudRepository<Imag,Integer> {
 
 }
